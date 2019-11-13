dc57690e2af1b05de140beb75c5ed648ec154ec5
pandabcd <smsubhodeep16@gmail.com>
2019-11-13 15:32:44 +0530

    Update

--- Assignment_3/q2/temp.c
+++ Assignment_3/q2/temp.c
@@ -2,53 +2,122 @@
 #include<stdlib.h>
 #include "mpi.h"
 
-#define arr_size 1000000
-#define query_size 50
+#define arr_size 40
+#define query_size 1
 
 int main(int argc, char **argv){
-	MPI_Status status;
-	MPI_Request request;
-
     int rank,size;
 
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
-  
+    
+    
+    MPI_Status status[size];
+    MPI_Request request_send[size], request_receive[size];
+
     
   	int recv;
 
     FILE *fptr;
-    char *filename = "q2-sample-input.txt";
+    // char *filename = "q2-sample-input.txt";
+    char *filename = "sample_input.txt";
+
     fptr = fopen(filename, "r");
     int *arr = (int *)malloc(arr_size*sizeof(int));
     int *query_arr = (int *)malloc(query_size*sizeof(int));
-    for(int i=0;i<size;i++){
-        fscanf(fptr,"%d ", &arr[i]);
-    }
-    for(int i=0;i<query_size;i++){
-        fscanf(fptr,"%d ", &query_arr[i]);
-    }
 
+    for(int i=0;i<arr_size;i++){
+        fscanf(fptr,"%d", &arr[i]);
+    }
+    // for(int i=0;i<query_size;i++){
+    //     fscanf(fptr,"%d ", &query_arr[i]);
+    // }
+
+    query_arr[0] = arr[33];
     
 
-    for(int i=0;i<size;i++){
-        if(i!=rank){
-            MPI_Isend(&rank,1,MPI_INT,i,86,MPI_COMM_WORLD,&request);
+    // for(int i=0;i<size;i++){
+    //     if(i!=rank){
+    //         MPI_Isend(&rank,1,MPI_INT,i,86,MPI_COMM_WORLD,&request);
+    //     }
+    // }
+
+    // MPI_Irecv(&recv,1,MPI_INT,MPI_ANY_SOURCE,86,MPI_COMM_WORLD,&request);
+    int dummy = 42;
+    int width = arr_size/size;
+
+    int not_found = -1;
+    
+    // printf("I am process %d and I am searching from %d to %d \n", rank, rank*width, (rank+1)*width);
+    for(int i=rank*width;i<(rank+1)*width;i++){
+        // if(rank==8){
+        //     printf("%d %d\n", arr[i], query_arr[0]);
+        // }
+        if(arr[i]==query_arr[0]){
+            printf("Element found by process %d at %d\n", rank, i);
+            not_found = 0;
+            for(int j=0;j<size;j++){
+                if(j!=rank){
+                    MPI_Isend(&i,1,MPI_INT,j,0,MPI_COMM_WORLD,&request_send[j]);
+                }
+            }
+            break;
         }
     }
 
-    MPI_Irecv(&recv,1,MPI_INT,MPI_ANY_SOURCE,86,MPI_COMM_WORLD,&request);
-    
-    int done=0;
-
-    while(!done){
-        printf("Not done!\n");
-        MPI_Test(&request,&done,&status);
+    if(not_found==-1){
+        for(int j=0;j<size;j++){
+            if(j!=rank){
+                MPI_Isend(&not_found,1,MPI_INT,j,0,MPI_COMM_WORLD,&request_send[j]);
+            }
+        }
+        printf("I am a useless thread. My id is %d (not found = %d)\n", rank, not_found);
     }
 
 
-    printf("I am process %d and I received from %d \n", rank, recv);
+    int j = 1-rank;
+    int done = 0;
+    MPI_Irecv(&recv,1,MPI_INT,j,0,MPI_COMM_WORLD,&request_receive[j]);
+    while(!done){
+        printf("Not done (process %d)\n", rank);
+        MPI_Test(&request_receive[j], &done, &status[j]);
+    }
+
+    printf("Received %d(process %d)\n", recv, rank);
+
+    // int done;
+    // for(int j=0;j<size;j++){
+    //     done = 0;
+    //     if(j!=rank){
+    //         while(!done){
+    //             MPI_Test(&request_receive[j], &done, &status[j]);
+        
+    //             MPI_Irecv(&recv,1,MPI_INT,j,0,MPI_COMM_WORLD,&request_receive[j]);
+    //             printf("Process %d received %d \n", rank, recv);
+    //             if(recv!=-1){
+    //                 printf("Element found bitch! @ (message by %d) %d \n", rank, recv);
+    //             }
+    //             done =1;
+    //         }
+    //     }
+    // }
+
+
+    // int done=0;
+    // int local_done =0 ;
+    // while(done!=size-1){
+    //     done = 0;
+    //     for(int i=0;i<size;i++){
+    //         MPI_Test(&request[i],&local_done,&status[i]);
+    //         if(local_done){
+    //             done++;
+    //         }
+    //     }
+    // }
+
+
+    // printf("I am process %d and I received %d \n", rank, recv);
 
     MPI_Finalize();
 
