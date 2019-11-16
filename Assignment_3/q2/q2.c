28aeacf8dde61f02e131dc4a40cdb91d93d0bc7f
pandabcd <smsubhodeep16@gmail.com>
2019-11-16 13:25:39 +0530

    update

--- Assignment_3/q2/q2.c
+++ Assignment_3/q2/q2.c
@@ -2,57 +2,110 @@
 #include<stdlib.h>
 #include "mpi.h"
 
-#define array_size 1000000
+#define arr_size 1000000
 #define query_size 50
 
-int main(int argc, char** argv){
-  MPI_Status status;
-  MPI_Request request;
+int main(int argc, char **argv){
+    int rank,size;
 
-  int rank,size;
-  int done,myfound,nvalues;
-  // Do we need to take double?
-  int *b = (int *)malloc(array_size*sizeof(int));
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    
+    
+    MPI_Status status[size];
+    MPI_Request request_send[size], request_receive[size];
 
-  int i,j,dummy;
-  FILE *infile;
+    
+    int recv;
 
-  MPI_Init(&argc,&argv);
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  MPI_Comm_size(MPI_COMM_WORLD,&size);
-  
-  myfound=0;
-  if (rank==0) {
-    infile=fopen("data","r");
-    for(i=0;i<size;++i) {
-      fscanf(infile,"%d",&b[i]);
+    FILE *fptr;
+    char *filename = "q2-sample-input.txt";
+   
+    fptr = fopen(filename, "r");
+    int *arr = (int *)malloc(arr_size*sizeof(int));
+    int *query_arr = (int *)malloc(query_size*sizeof(int));
+    int *result_arr = (int *)malloc(query_size*sizeof(int));
+
+    for(int i=0;i<arr_size;i++){
+        fscanf(fptr,"%d", &arr[i]);
     }
-  }
 
-  MPI_Bcast(b,size,MPI_INT,0,MPI_COMM_WORLD);
-  MPI_Irecv(&dummy,1,MPI_INT,MPI_ANY_SOURCE,86,MPI_COMM_WORLD,&request);
-  MPI_Test(&request,&done,&status);
+    for(int i=0;i<query_size;i++){
+        fscanf(fptr,"%d", &query_arr[i]);
+        result_arr[i] = -1;
+    }
+
+    // for(int i=0;i<query_size;i++){
+    //     printf("Query : %d (Process %d)\n", query_arr[i], rank);
+    // }
+    
+
+    
+    int width = arr_size/size;
+    int not_found = -1;
+    
+    // For every query
+    for(int q=0;q<query_size;q++){
+        int found = 0;
+        // Every thread finds it part
+        for(int i=rank*width;i<(rank+1)*width;i++){
+            if(arr[i]==query_arr[q]){
+                result_arr[q] = i;
+                found = 1;
+                // Send result to every processor if found
+                for(int j=0;j<size;j++){
+                    MPI_Isend(&i,1,MPI_INT,j,q,MPI_COMM_WORLD,&request_send[j]);
+                }
+                break;
+            }
+        }
+        if(!found){
+          int dummy = -1;
+          for(int j=0;j<size;j++){
+            MPI_Isend(&dummy,1,MPI_INT,j,q,MPI_COMM_WORLD,&request_send[j]);
+          }
+        }
+    }
+   
+
+    int final_result[query_size];
+    for(int i=0;i<query_size;i++){
+      final_result[i] = -1;
+    }
+    
+    int receive_store[query_size][size];
+    for(int i=0;i<query_size;i++){
+      for(int j=0;j<size;j++){
+        receive_store[i][j] = 0;
+      }
+    }    
+
+    for(int q=0;q<query_size;q++){
+      for(int j=0;j<size;j++){
+          int done = 0;
+
+          MPI_Irecv(&receive_store[q][j],1,MPI_INT,j,q,MPI_COMM_WORLD,&request_receive[j]);
+          while(!done){
+              MPI_Test(&request_receive[j], &done, &status[j]);
+          }
+
+      }
+    }
+
+
+    if(rank==0){
+        for(int q=0;q<query_size;q++){
+          for(int j=0;j<size;j++){
+            printf("%d ", receive_store[q][j]);
+          }
+            printf("\n");
+          }
+    }
+    
+
   
-  nvalues=array_size/size;
-  int low = rank*nvalues;
-  int high = (rank +1)*nvalues;
-  i=low;
-  
-  printf("Searching in %d and %d\n", low, high);
-  // while(!done && i>=low && i<high) {
-  //     if (b[i]==11) {
-  //       dummy=123;
-  //       for(j=0;j<size;++j) {
-  //         MPI_Send(&dummy,1,MPI_INT,j,86,MPI_COMM_WORLD);
-  //       }
-  //       printf("P:%d found it at global index %d\n",rank,i);
-  //       myfound=1;
-  //     }
-  //     MPI_Test(&request,&done,&status);
-  //     ++i;
-  // }
-  // if (!myfound) {
-  //   printf("P:%d stopped at global index %d\n",rank,i-1);
-  // }
-  MPI_Finalize();
+    MPI_Finalize();
+
+  return 0;
 }
